0ea51c07284da0d02b8207bd7c3bbdd46575d76e
SofiPolak <sofi_polak@hotmail.com>
2024-06-11 21:05:24 -0300

    Estructura

---
+++ practicaIntegradora2/src/config/passport.config.js
@@ -0,0 +1,91 @@
+import passport from "passport";
+import local from 'passport-local'
+import userService from '../dao/models/users.model.js'
+import { createHash, isValidPassword } from "../utils.js";
+import GitHubStrategy from 'passport-github2';
+
+
+const LocalStrategy = local.Strategy
+
+const initializePassport = () => {
+
+    passport.use('register', new LocalStrategy(
+        { passReqToCallback: true, usernameField: 'email' }, async (req, username, password, done) => {
+            const { first_name, last_name, email, age } = req.body
+            try {
+                let user = await userService.findOne({ email: username })
+                if (user) {
+                    console.log("El usuario ya existe")
+                    return done(null, false)
+                }
+                const newUser = {
+                    first_name,
+                    last_name,
+                    email,
+                    age,
+                    password: createHash(password)
+                }
+                let result = await userService.create(newUser)
+                return done(null, result)
+            } catch (error) {
+                return done("Error al obtener el usuario" + error)
+            }
+        }
+    ))
+
+    passport.use('github', new GitHubStrategy({
+        clientID: "Iv23liyy5dDp25mioyfc",
+        clientSecret: "2eade64c4c72cd3cdf2fe603e279a3168bf6294a",
+        callbackURL: "http://localhost:8080/api/sessions/githubcallback"
+    }, async (accessToken, refreshToken, profile, done) => {
+        try {
+            console.log(profile)
+            let user = await userService.findOne({ email: profile._json.email })
+            if (!user) {
+                let newUser = {
+                    first_name: profile._json.name,
+                    last_name: "",
+                    age: 20,
+                    email: profile._json.email,
+                    password: ""
+                }
+                let result = await userService.create(newUser)
+                done(null, result)
+            }
+            else {
+                done(null, user)
+            }
+        } catch (error) {
+            return done(error)
+        }
+    }))
+
+    passport.serializeUser((user, done) => {
+        done(null, user._id)
+    })
+
+    passport.deserializeUser(async (id, done) => {
+        let user = await userService.findById(id)
+        done(null, user)
+    })
+
+
+    passport.use('login', new LocalStrategy({ usernameField: 'email' }, async (username, password, done) => {
+        try {
+            const user = await userService.findOne({ email: username })
+            if (!user) {
+                console.log("El usuario no existe")
+                return done(null, user)
+            }
+            if (!isValidPassword(user, password)) return done(null, false)
+            return done(null, user)
+        } catch (error) {
+            return done(error)
+        }
+    }))
+
+
+}
+
+
+export default initializePassport
---
+++ practicaIntegradora2/src/routes/api/products.router.js
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import ProductManager from "../../dao/db/productManager.js";
+const productManager = new ProductManager();
+const router = Router();
+
+router.get('/', async (req, res) => {
+    const result = await productManager.checkProducts(req.query);
+    res.send({ result: "success", payload: result })
+})
+
+router.get('/:pid', async (req, res) => {
+    let { pid } = req.params;
+    const result = await productManager.getProductById(pid);
+    res.send({ result: "success", payload: result })
+})
+
+router.post('/', async (req, res) => {
+    let { title, description, price, thumbnail, code, stock, available, category } = req.body
+    if (!title || !description || !price || ! thumbnail || !code || !stock || !available || !category) {
+        res.send({ status: "error", error: "Faltan parametros" })
+    }
+    const result = await productManager.addProduct(title, description, price, thumbnail, code, stock, available, category);
+    res.send({ result: "success", payload: result });
+})
+
+router.put('/:pid', async (req, res) => {
+    let { pid } = req.params
+    let productToReplace = req.body
+    if (!productToReplace.title || !productToReplace.description || !productToReplace.price || !productToReplace.thumbnail || !productToReplace.code || !productToReplace.stock || !productToReplace.category) {
+        res.send({ status: "error", error: "Parametros no definidos" })
+    }
+    const result = await productManager.updateProduct(pid,productToReplace);
+    res.send({ result: "success", payload: result });
+})
+
+router.delete('/:pid', async (req, res) => {
+    let { pid } = req.params
+    const result = await productManager.deleteProduct(pid);
+    res.send({ result: "success", payload: result });
+})
+
+
+export default router;
